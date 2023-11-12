0dd1a8b3ce2890fc387a5fbb201476329a387ed7
sShakar <shakarkosar01@gmail.com>
2023-11-12 10:12:10 +0300

    chore: files reorganization

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
 
+import { options, Quasar } from '@/plugins/quasar';
+import './assets/scss/main.scss';
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(Quasar, options);
 
 app.mount('#app');
--- src/plugins/quasar.ts
+++ src/plugins/quasar.ts
@@ -1,5 +1,5 @@
-import { Dialog, Loading, Notify, Quasar, SessionStorage, QBtn, QInput, QSelect } from 'quasar';
-import type { QuasarPluginOptions, QInputProps, QBtnProps, QSelectProps } from 'quasar';
+import { Dialog, Loading, Notify, Quasar, SessionStorage } from 'quasar';
+import type { QuasarPluginOptions } from 'quasar';
 import quasarIconSet from 'quasar/icon-set/material-icons-outlined';
 // Import icon libraries
 import '@quasar/extras/material-icons-outlined/material-icons-outlined.css';
@@ -7,39 +7,6 @@ import '@quasar/extras/mdi-v6/mdi-v6.css';
 
 // Import Quasar css
 import 'quasar/src/css/index.sass';
-
-// Default Quasar components props
-const defaultQBtnProps: Partial<QBtnProps> = {
-	unelevated: true,
-	square: true
-};
-
-const defaultQInputProps: Partial<QInputProps> = {
-	outlined: true,
-	clearable: true,
-	dense: true,
-	color: 'grey-8'
-};
-
-const defaultQSelectProps: Partial<QSelectProps> = {
-	outlined: true,
-	clearable: true,
-	dense: true,
-	color: 'grey-8'
-};
-
-assignDefaultProps(QBtn.props, defaultQBtnProps);
-assignDefaultProps(QInput.props, defaultQInputProps);
-assignDefaultProps(QSelect.props, defaultQSelectProps);
-
-function assignDefaultProps(props: any, defaultPropValues: any) {
-	Object.entries(defaultPropValues).forEach(([prop, value]) => {
-		props[prop] =
-			Array.isArray(props[prop]) === true || typeof props[prop] === 'function'
-				? { type: props[prop], default: value }
-				: { ...props[prop], default: value };
-	});
-}
 
 const options: Partial<QuasarPluginOptions> = {
 	plugins: {
